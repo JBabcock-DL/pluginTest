3f51166cac9c15c42bca7bc98ffebbdef4d759ff
JBabcock-DL <jacob.babcock@detroitlabs.com>
2026-07-18 11:18:32 -0400

    FigHub: resolve design drift (push)

--- components/badge/Badge.figma.tsx
+++ components/badge/Badge.figma.tsx
@@ -7,7 +7,7 @@ import { Badge } from '@/components/ui/badge';
  */
 figma.connect(
   Badge,
-  'https://www.figma.com/design/KgRowhi11oXBNoMwBHyzLG/Untitled?node-id=1-559',
+  'https://www.figma.com/design/UlSZwfPtQcbBxam3nrlUey/Untitled?node-id=1-559',
   {
     props: {
       type: figma.enum('type', { Variable: 'variable', Syntax: 'syntax', Value: 'value', GroupAnnotation: 'group-annotation' }),
